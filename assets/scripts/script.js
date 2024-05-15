9dcc6ca4906b746bff469cfb0862640a029fda87
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-05-15 17:16:13 +0100

    Add for loop with an if statement containing modulo that checks for the remainder of 3 for fizz, 5 for buzz and both for Fizz Buzz

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,3 +2,16 @@ let userNumberPrompt = prompt(
   "Let's play Fizz Buzz! Enter your number! Then check the console to play."
 );
 console.log("You entered: " + userNumberPrompt);
+
+for (i = 1; i <= userNumberPrompt; i++) {
+  console.log(i);
+  if (i % 3 === 0) {
+    console.log(i + " Fizz");
+  }
+  if (i % 5 === 0) {
+    console.log(i + " Buzz");
+  }
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i + " Fizz Buzz");
+  }
+}
