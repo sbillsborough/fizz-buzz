11ace45b8c2b9ee70ba5555699889b9c1fec5317
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-05-15 17:27:12 +0100

    Refactor the logic
    Moved the && conditional to the top so this is checked first and prints Fizz Buzz instead of Fizz or Buzz when a number is divisible by 3 or 5
    Moved the i console.log into the else to prevent logging off double numbers eg.
    3
    3 Fizz
    5
    5 Buzz

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,14 +4,14 @@ let userNumberPrompt = prompt(
 console.log("You entered: " + userNumberPrompt);
 
 for (i = 1; i <= userNumberPrompt; i++) {
-  console.log(i);
-  if (i % 3 === 0) {
-    console.log(i + " Fizz");
-  }
-  if (i % 5 === 0) {
-    console.log(i + " Buzz");
-  }
+  // console.log(i);
   if (i % 3 === 0 && i % 5 === 0) {
     console.log(i + " Fizz Buzz");
+  } else if (i % 3 === 0) {
+    console.log(i + " Fizz");
+  } else if (i % 5 === 0) {
+    console.log(i + " Buzz");
+  } else {
+    console.log(i);
   }
 }
